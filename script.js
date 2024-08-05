abd93193b8be04831a53b46be300be7b93f19b9a
Muhammad Bilal <seoguestposter4u@gmail.com>
2024-08-05 21:26:00 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("HELLO WORLD");
+let num1=2;
+let num2=4;
+console.log(num1+num2);
+console.log(num1);
+console.log(num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
