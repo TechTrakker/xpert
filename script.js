6985d6561535c4212b1b07078dedcd19390f1c26
Muhammad Bilal <seoguestposter4u@gmail.com>
2024-08-06 15:17:54 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-console.log("HELLO WORLD");
+ console.log("HELLO WORLD");
 let num1=2;
 let num2=4;
-console.log(num1+num2);
-console.log(num1);
-console.log(num2);
-console.log(num1-num2);
-console.log(num1*num2);
-console.log(num1/num2);
+// let sum=num1+num2;
+// let sum=num1-num2;
+// let sum=num1*num2;
+let sum=num1/num2;
+console.log(sum);
